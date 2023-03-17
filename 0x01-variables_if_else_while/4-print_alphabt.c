dae379057c4d089bf395d6ddbf8eb34a881ed6f3
Mohamed3li454 <mohhamed4413@gmail.com>
2023-03-17 17:37:58 -0400

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	char alp[26] = "abcdfghijklmnoprstuvwxyz";
+	char alp[25] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 25; i++)
 	{
 		putchar(alp[i]);
 	}
