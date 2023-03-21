599800505cca148900999cec3ecb9a6413762db8
Mohamed3li454 <mohhamed4413@gmail.com>
2023-03-21 07:25:41 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+void pu()
+{
+	printf("_putchar");
+	return 0;
+}
+int main(void)
+{
+	    pu();
+}
